2e616e4906dd426e06b3963dfcc8956f6d49baf1
ichlasulamalgit <id.ichlasulamal@gmail.com>
2020-08-26 18:54:16 +0700

    Success create login and register

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from '../api/axiosInstance'
+import router from '../router/'
 
 Vue.use(Vuex)
 
@@ -9,6 +11,32 @@ export default new Vuex.Store({
   mutations: {
   },
   actions: {
+    postRegister (_, payload) {
+      axios.post('/users/register', {
+        email: payload.email,
+        password: payload.password
+      })
+        .then(({ data }) => {
+          console.log('===================')
+          console.log('Berhasil Register')
+          console.log(data)
+          console.log('===================')
+        })
+    },
+    postLogin (_, payload) {
+      axios.post('/users/login', {
+        email: payload.email,
+        password: payload.password
+      })
+        .then(({ data }) => {
+          console.log('===================')
+          console.log('Berhasil Login')
+          console.log(data)
+          console.log('===================')
+          localStorage.setItem('token', data.token)
+          router.push({ path: '/' })
+        })
+    }
   },
   modules: {
   }
